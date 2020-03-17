d3dc6eba6c643ef58154d0698f07edc2c425fa5b
dss19 <samoylov82@gmail.com>
2020-03-17 23:31:41 +0300

    реализовал поиск

--- src/components/search-panel/search-panel.jsx
+++ src/components/search-panel/search-panel.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './search-panel.css';
+export default class SearchPanel extends Component {
+  
+  state = {
+    term: ''
+  };
 
-const SearchPanel = () => {
-  return (
-    <input type="text"
-              className="form-control search-input"
-              placeholder="type to search" />
-  );
+  onSearchCange = (e) => {
+    const term = e.target.value;
+    this.setState({term});
+    this.props.onSearchChange(term);
+  };
+  
+  render() {  
+    return (
+      <input type="text"
+        className="form-control search-input"
+        placeholder="type to search"
+        value={this.state.term}
+        onChange={this.onSearchCange} />
+    );
+  };  
 };
 
-export default SearchPanel;
+
